73da815339ee7bbdcb50c53f84efa08e1c304326
jwm <wzskqjh@gmail.com>
2024-11-19 15:54:58 +0800

    add fish

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,3 +1,9 @@
 # all alias
+alias c="clear"
+
+
+
 alias web='source /home/jwm/project/connection/web/bin/activate.fish'
 alias geo='source /home/jwm/project/red\ book/geo\ science/geo/bin/activate.fish'
+alias spider='source /home/jwm/project/万总的爬虫/spider/bin/activate.fish'
+alias spider='source /home/jwm/project/万总的爬虫/spider/bin/activate.fish'
