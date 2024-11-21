dbcb8e20b99dc8d93d0ba1de97476c57c28a28e6
jwm <wzskqjh@gmail.com>
2024-11-21 14:51:34 +0800

    change alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,9 +1,6 @@
 # all alias
 alias c="clear"
-
+alias n="neofetch"
 
 
 alias web='source /home/jwm/project/connection/web/bin/activate.fish'
-alias geo='source /home/jwm/project/red\ book/geo\ science/geo/bin/activate.fish'
-alias spider='source /home/jwm/project/万总的爬虫/spider/bin/activate.fish'
-alias spider='source /home/jwm/project/万总的爬虫/spider/bin/activate.fish'
